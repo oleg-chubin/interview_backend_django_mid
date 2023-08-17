a955df62cb4f1c5c51778fa3d80c92ed6dbfd907
Oleg Chubin <o_chubin@lesta.group>
2023-08-17 20:53:36 +0300

    fix codestyle

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
+
 
 # Create your views here.
 class OrderListCreateView(generics.ListCreateAPIView):
